b092f903255b8bc40f782f59e6b1f2d194e08a43
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-12-08 15:02:26 +0100

    feat: update build files

--- flake.nix
+++ flake.nix
@@ -1,29 +1,54 @@
 {
   inputs = {
+    fenix.url = "github:nix-community/fenix";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    fenix,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = (import nixpkgs) {
           inherit system;
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.wasm32-unknown-unknown.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        naerskBuildPackage = target: args:
+          naersk'.buildPackage (
+            args
+            // {CARGO_BUILD_TARGET = target;}
+          );
       in rec {
         # For `nix build` & `nix run`:
-        packages.default = naersk'.buildPackage {
+        defaultPackage = packages.wasm32-unknown-unknown;
+
+        packages.wasm32-unknown-unknown = naerskBuildPackage "wasm32-unknown-unknown" {
           src = ./.;
         };
 
         # For `nix develop`:
         devShell = pkgs.mkShell rec {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          inputsFrom = with packages; [wasm32-unknown-unknown];
 
-          buildInputs = with pkgs; [ pkg-config glib cairo gtk2 libsoup_3 webkitgtk_4_1 ];
+          buildInputs = with pkgs; [pkg-config glib cairo gtk2 libsoup_3 webkitgtk_4_1 openssl];
 
           LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
         };

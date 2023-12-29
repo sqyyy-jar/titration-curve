2c4a0599b099811536e2064cc969cb3972880e62
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-12-29 23:02:02 +0100

    feat: implement graph

--- src/app.rs
+++ src/app.rs
@@ -1,29 +1,49 @@
+pub mod diagram;
 pub mod options;
+pub mod strings;
 
-use std::rc::Rc;
+use std::{
+    sync::{mpsc::Receiver, Arc},
+    time::{Duration, Instant},
+};
 
 use iced::{
     executor,
-    widget::{button, checkbox, column, container, row, text},
-    Application, Command, Element, Length, Theme,
+    time::every,
+    widget::{button, checkbox, column, container, row, svg, svg::Handle, text},
+    Application, Command, ContentFit, Element, Length, Subscription, Theme,
 };
-use rfd::FileDialog;
 
-use self::options::Options;
+use crate::{
+    util::*,
+    worker::{Output, Response, Signal, Worker},
+};
+
+use self::{
+    options::Options,
+    strings::{BUTTON_SELECT_FILE, MESSAGE_NO_CONTENT, OPTION_COLORED, OPTION_DARK, WINDOW_TITLE},
+};
 
 #[derive(Clone, Debug)]
 pub enum Message {
-    /// Set the `dark` option
+    /// Sets the `dark` option.
     SetDark(bool),
-    /// Set the `colored` options
+    /// Sets the `colored` option.
     SetColored(bool),
-    /// Open a file
+    /// Opens a file dialog.
     SelectFile,
+    /// Processes the response queue.
+    Update(Instant),
 }
 
-#[derive(Default)]
 pub struct TitrationCurve {
-    options: Rc<Options>,
+    options: Options,
+    worker: Arc<Worker>,
+    response_receiver: Receiver<Response>,
+    /// The content of the window.
+    ///
+    /// Either a graph of the output or a message.
+    content: Either<Arc<Output>, String>,
 }
 
 impl Application for TitrationCurve {
@@ -33,26 +53,34 @@ impl Application for TitrationCurve {
     type Theme = Theme;
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (Self::default(), Command::none())
+        let (worker, response_receiver) = Worker::spawn();
+        let app = Self {
+            options: Options::default(),
+            worker,
+            response_receiver,
+            content: Right(MESSAGE_NO_CONTENT.into()),
+        };
+        (app, Command::none())
     }
 
     fn title(&self) -> String {
-        "Titrationskurve".into()
+        WINDOW_TITLE.into()
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            Message::SetDark(dark) => self.options.set_dark(dark),
-            Message::SetColored(colored) => self.options.set_colored(colored),
-            Message::SelectFile => {
-                if let Some(file) = FileDialog::new()
-                    .add_filter(
-                        "Tabelle",
-                        &["xls", "xlsx", "xlsm", "xlsb", "xla", "xlam", "ods"],
-                    )
-                    .pick_file()
-                {
-                    println!("{file:?}");
+            Message::SetDark(dark) => self.options.dark = dark,
+            Message::SetColored(colored) => self.options.colored = colored,
+            Message::SelectFile => self.worker.send_signal(Signal::FileDialog),
+            Message::Update(_) => {
+                while let Ok(response) = self.response_receiver.try_recv() {
+                    match response {
+                        Response::Unload => self.content = Right(MESSAGE_NO_CONTENT.into()),
+                        Response::Output(output) => self.content = Left(output),
+                        Response::Error(err) => {
+                            self.content = Right(format!("Ein Fehler ist aufgetreten: {err}"))
+                        }
+                    }
                 }
             }
         }
@@ -61,10 +89,10 @@ impl Application for TitrationCurve {
 
     fn view(&self) -> Element<Self::Message> {
         let controls = {
-            let dark_toggle = checkbox("Dunkel", self.options.is_dark(), Message::SetDark);
+            let dark_toggle = checkbox(OPTION_DARK, self.options.dark, Message::SetDark);
             let colored_toggle =
-                checkbox("Gefärbt", self.options.is_colored(), Message::SetColored);
-            let file_button = button("Datei auswählen").on_press(Message::SelectFile);
+                checkbox(OPTION_COLORED, self.options.colored, Message::SetColored);
+            let file_button = button(BUTTON_SELECT_FILE).on_press(Message::SelectFile);
             container(
                 column![dark_toggle, colored_toggle, file_button]
                     .spacing(5)
@@ -73,21 +101,36 @@ impl Application for TitrationCurve {
             .width(Length::Fixed(110.0))
             .height(Length::Fill)
         };
-        let graph = container(text("TODO"))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center_x()
-            .center_y()
-            .padding(10);
-        let content = row![controls, graph];
-        content.into()
+        let content = match &self.content {
+            Left(output) => {
+                let svg_text = diagram::render_graph(&self.options, &output);
+                let handle = Handle::from_memory(svg_text.into_bytes());
+                container(
+                    svg(handle)
+                        .width(Length::Fill)
+                        .height(Length::Fill)
+                        .content_fit(ContentFit::Contain),
+                )
+            }
+            Right(message) => container(text(message)),
+        }
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x()
+        .center_y()
+        .padding(10);
+        row![controls, content].into()
     }
 
     fn theme(&self) -> Self::Theme {
-        if self.options.is_dark() {
+        if self.options.dark {
             Theme::Dark
         } else {
             Theme::Light
         }
+    }
+
+    fn subscription(&self) -> Subscription<Self::Message> {
+        Subscription::batch([every(Duration::from_millis(500)).map(Message::Update)])
     }
 }
